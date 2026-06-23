e14ee094b78d2af3cdc25a90b5db41ca9f535fcf
Amed vanegas <amedvanegas368@gmail.com>
2026-06-22 20:41:25 -0500

    creacion de rutas, y componentes hijos de APP, header y footer y se relacionan con el resto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: 'home', component: Home }, //ruta carga automaticamente 
+
+    // rutas cargan solo cuando se carga la url 
+
+  {
+    path: 'brochure',
+    loadComponent: () => import('./features/brochure/brochure')
+  },
+  {
+    path: 'about-us',
+    loadComponent: () => import('./features/about-us/about-us')
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./features/contact/contact'),
+  },
+  {
+    path: '404',
+    loadComponent: () =>
+      import('./features/page-not-found/page-not-found')
+  },
+  //Redirecciones
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '404', pathMatch: 'full' },
+];
