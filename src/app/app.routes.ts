3fb4f1c4f00de4209e87918453ad067ad0dda7ef
Johan Daniel Patiño <johansr243@gmail.com>
2026-06-22 20:41:24 -0500

    [+] FrotEnd/components/header/footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './app/features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'about', loadComponent: () => import('./app/features/about/about') .then( m => m.About )},
+  { path: 'brochure', loadComponent: () => import('./app/features/brochure/brochure') .then( m => m.Brochure) },
+  { path: '404', loadComponent: () => import('./app/features/page-not-found/page-not-found')},
+  // Redirecciones
+    {path:'', redirectTo: 'home', pathMatch: 'full'},
+    {path: '++', redirectTo: '404', pathMatch: 'full'}
+];
---
+++ src/app/app/features/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { routes } from '../../../app.routes';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
