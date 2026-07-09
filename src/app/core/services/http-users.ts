1534da5b0e706c65a98f3f9960cbcf7fc3e4e2dd
Amed vanegas <amedvanegas368@gmail.com>
2026-07-09 18:35:48 -0500

    formulario

--- src/app/core/services/http-users.ts
+++ src/app/core/services/http-users.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
+import { map } from 'rxjs';
 
 @Service()
 export class HttpUsers {
@@ -8,6 +9,17 @@ export class HttpUsers {
   private http = inject(HttpClient);
 
   getUsers() {
-    return this.http.get('http://localhost:3000/api/users');
+    return this.http.get<any>('http://localhost:3000/api/users').pipe(
+      map((res) => {
+        return res.data;
+      }),
+    );
+  }
+
+  createUser(newUser:any){
+
+    return this.http.post<any>('http://localhost:3000/api/users', newUser)
+
+
   }
 }
