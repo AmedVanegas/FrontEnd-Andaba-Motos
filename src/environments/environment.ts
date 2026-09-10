9e7c51786d7de93556823e0d093b73eca275e38e
Amed vanegas <amedvanegas368@gmail.com>
2026-09-09 19:41:38 -0500

    ya se termini

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
- apiUrl: 'http://ec2-18-118-161-162.us-east-2.compute.amazonaws.com:3000/api' ,
-    filesUrl:'http://ec2-18-118-161-162.us-east-2.compute.amazonaws.com:3000'
+  production: true,
+  apiUrl: 'http://ec2-18-189-31-135.us-east-2.compute.amazonaws.com:3000/api',
+  filesUrl: 'http://ec2-18-189-31-135.us-east-2.compute.amazonaws.com:3000',
 };
